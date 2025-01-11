a87766ed5a894bc58b4d473df0fb2709acb5f886
sjhello <sjhello235@gmail.com>
2025-01-11 14:38:58 +0900

    init

---
+++ src/test/java/io/sjhello/refactoring/SjhelloRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sjhello.refactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjhelloRefactoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
