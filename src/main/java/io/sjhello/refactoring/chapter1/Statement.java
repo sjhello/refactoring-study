0abf2eeffec9b9c1b3873a76a09114f89d6e6d50
sjhello <sjhello235@gmail.com>
2025-01-18 17:13:49 +0900

    refactor: volumeCredits 문장 슬라이드 & 반복문 쪼개기

--- src/main/java/io/sjhello/refactoring/chapter1/Statement.java
+++ src/main/java/io/sjhello/refactoring/chapter1/Statement.java
@@ -7,19 +7,20 @@ public class Statement {
 
 	public String statement(List<Invoice> invoices, Map<String, Play> playMap) {
 		int totalAmount = 0;
-		int volumeCredits = 0;
 		Invoice invoice = invoices.get(0);
 
 		StringBuilder result = new StringBuilder(String.format("청구 내역 (고객명: %s)\n", invoice.getCustomer()));
 
 		for (Performance performance : invoice.getPerformances()) {
-			// 포인트를 적립한다.
-			volumeCredits += volumeCreditsFor(performance, playMap);
-
 			// 청구내역을 출력한다
 			result.append(String.format("%s: $%d (%d석)\n", playFor(playMap, performance).getName(), amountFor(performance, playMap)
 				/ 100, performance.getAudience()));
 			totalAmount += amountFor(performance, playMap);
+		}
+
+		int volumeCredits = 0;
+		for (Performance performance : invoice.getPerformances()) {
+			volumeCredits += volumeCreditsFor(performance, playMap);
 		}
 
 		result.append(String.format("총액: $%d\n", totalAmount / 100));
@@ -29,7 +30,10 @@ public class Statement {
 
 	private int volumeCreditsFor(Performance performance, Map<String, Play> playMap) {
 		int volumeCredits = 0;
+
+		// 포인트를 적립한다.
 		volumeCredits += Math.max(performance.getAudience() - 30, 0);
+
 		// 희극 관객 5명마다 추가포인트를 제공한다.
 		if (playFor(playMap, performance).getType().equals(PlayType.COMEDY)) {
 			volumeCredits += Math.floor(performance.getAudience() / 5);
