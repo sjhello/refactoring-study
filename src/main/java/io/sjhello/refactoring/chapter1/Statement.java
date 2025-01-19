a9064be9ebe2a6f330e1ea0175d4122ef4390d51
sjhello <sjhello235@gmail.com>
2025-01-19 22:55:21 +0900

    refactor & test: html 버전 작성, html 버전 테스트 추가

--- src/main/java/io/sjhello/refactoring/chapter1/Statement.java
+++ src/main/java/io/sjhello/refactoring/chapter1/Statement.java
@@ -9,6 +9,26 @@ public class Statement {
 		StatementData statementData = new StatementData(invoices.get(0), playMap);
 		return renderPlainText(statementData);
 	}
+
+	public String htmlStatement(List<Invoice> invoices, Map<String, Play> playMap) {
+		StatementData statementData = new StatementData(invoices.get(0), playMap);
+		return renderHtml(statementData);
+	}
+
+	public String renderHtml(StatementData statementData) {
+		StringBuilder result = new StringBuilder(String.format("<h1> 청구내역 (고객명: %s)\n </h1>", statementData.getInvoice().getCustomer()));
+		result.append("<table> \n");
+		result.append("<tr><th> 연극 </th> <th>좌석 수</th> <th>금액</th>");
+		for (Performance performance : statementData.getInvoice().getPerformances()) {
+			result.append(String.format("<tr><td> %s: </td> <td> $%d </td> <td> %d석 </td></tr>\n",statementData.playFor(performance).getName(), statementData.amountFor(performance) / 100, performance.getAudience()));
+		}
+		result.append("</table>\n");
+
+		result.append(String.format("총액: $%d\n", statementData.totalAmount()));
+		result.append(String.format("적립 포인트: %d점", statementData.totalVolumeCredits()));
+		return result.toString();
+	}
+
 	private String renderPlainText(StatementData statementData) {
 		StringBuilder result = new StringBuilder(String.format("청구 내역 (고객명: %s)\n", statementData.getInvoice().getCustomer()));
 		for (Performance performance : statementData.getInvoice().getPerformances()) {
--- src/test/java/io/sjhello/refactoring/chapter1/StatementTest.java
+++ src/test/java/io/sjhello/refactoring/chapter1/StatementTest.java
@@ -47,6 +47,28 @@ class StatementTest {
 			적립 포인트: 47점""");
 	}
 
+	@Test
+	void statementTest2() throws IOException {
+		// given
+		Statement statement = new Statement();
+
+		List<Invoice> invoices = findInvoices();
+		Map<String, Play> playMap = findPlays();
+
+		// then
+		String result = statement.htmlStatement(invoices, playMap);
+		String expected = """
+			<h1> 청구내역 (고객명: BigCo)
+			 </h1><table>\s
+			<tr><th> 연극 </th> <th>좌석 수</th> <th>금액</th><tr><td> Hamlet: </td> <td> $650 </td> <td> 55석 </td></tr>
+			<tr><td> As You Like It: </td> <td> $580 </td> <td> 35석 </td></tr>
+			<tr><td> Othello: </td> <td> $500 </td> <td> 40석 </td></tr>
+			</table>
+			총액: $173000
+			적립 포인트: 47점""";
+		assertThat(result).isEqualTo(expected);
+	}
+
 	private Map<String, Play> findPlays() throws IOException {
 		InputStream playMapStream = this.classLoader.getResourceAsStream("chapter1/plays.json");
 		if (playMapStream == null) {
