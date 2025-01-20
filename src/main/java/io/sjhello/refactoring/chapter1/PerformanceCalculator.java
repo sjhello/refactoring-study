d48f18c7c635d4be5f506c0235caa549f6659164
sjhello <sjhello235@gmail.com>
2025-01-21 00:07:16 +0900

    refactor: 적립 포인트 계산 로직 분리

--- src/main/java/io/sjhello/refactoring/chapter1/PerformanceCalculator.java
+++ src/main/java/io/sjhello/refactoring/chapter1/PerformanceCalculator.java
@@ -32,4 +32,17 @@ public class PerformanceCalculator {
         }
         return result;
     }
+
+    public int volumeCredits() {
+        int result = 0;
+
+        // 포인트를 적립한다.
+        result += Math.max(performance.getAudience() - 30, 0);
+
+        // 희극 관객 5명마다 추가포인트를 제공한다.
+        if (this.play.getType().equals(PlayType.COMEDY)) {
+            result += Math.floor(performance.getAudience() / 5);
+        }
+        return result;
+    }
 }
--- src/main/java/io/sjhello/refactoring/chapter1/StatementData.java
+++ src/main/java/io/sjhello/refactoring/chapter1/StatementData.java
@@ -51,15 +51,6 @@ public class StatementData {
 	}
 
 	private int volumeCreditsFor(Performance performance) {
-		int result = 0;
-
-		// 포인트를 적립한다.
-		result += Math.max(performance.getAudience() - 30, 0);
-
-		// 희극 관객 5명마다 추가포인트를 제공한다.
-		if (playFor(performance).getType().equals(PlayType.COMEDY)) {
-			result += Math.floor(performance.getAudience() / 5);
-		}
-		return result;
+		return new PerformanceCalculator(performance, playFor(performance)).volumeCredits();
 	}
 }
