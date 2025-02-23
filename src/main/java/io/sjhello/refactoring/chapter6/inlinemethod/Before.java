bde7e36fa7648c325e18098bf030d85c58072d74
sjhello <sjhello235@gmail.com>
2025-02-23 23:23:25 +0900

    6.2 함수 인라인하기 - 간단한 경우

--- src/main/java/io/sjhello/refactoring/chapter6/inlinemethod/Before.java
+++ src/main/java/io/sjhello/refactoring/chapter6/inlinemethod/Before.java
@@ -3,10 +3,7 @@ package io.sjhello.refactoring.chapter6.inlinemethod;
 public class Before {
 
     public int rating(Driver aDriver) {
-        return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
+        return aDriver.getNumberOfLateDeliveries() > 5 ? 2 : 1;
     }
 
-    private boolean moreThanFiveLateDeliveries(Driver aDriver) {
-        return aDriver.getNumberOfLateDeliveries() > 5;
-    }
 }
