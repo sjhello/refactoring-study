1aa080195f8f38cd6c9215f928fb51ddaafa225d
sjhello <sjhello235@gmail.com>
2025-03-09 21:45:44 +0900

    8.2 필드 옮기기 - discountRate를 CustomerContract로 옮기기

--- src/main/java/io/sjhello/refactoring/chapter8/movefield/Customer.java
+++ src/main/java/io/sjhello/refactoring/chapter8/movefield/Customer.java
@@ -5,29 +5,24 @@ import java.time.LocalDate;
 public class Customer {
 
     private String name;
-    private double discountRate;
     private CustomerContract contract;
 
     public Customer(String name, double discountRate) {
         this.name = name;
-        setDiscountRate(discountRate);
-        this.contract = new CustomerContract(LocalDate.now());
-    }
-
-    private void setDiscountRate(double discountRate) {
-        this.discountRate = discountRate;
+        this.contract = new CustomerContract(LocalDate.now(), discountRate);
+        contract.setDiscountRate(discountRate);
     }
 
     public double getDiscountRate() {
-        return discountRate;
+        return contract.getDiscountRate();
     }
 
     public void becomePreferred() {
-        setDiscountRate(getDiscountRate() + 0.03);
+        contract.setDiscountRate(contract.getDiscountRate() + 0.03);
         // 다른 멋진 일들
     }
 
     public double applyDiscount(double amount) {
-        return amount - (amount * getDiscountRate());
+        return amount - (amount * contract.getDiscountRate());
     }
 }
