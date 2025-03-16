ab34475d0a6f4c2dadecc7b63785546b08f0e7f4
sjhello <sjhello235@gmail.com>
2025-03-16 18:56:35 +0900

    8.7 반복문 쪼개기 - 리팩터링 전

---
+++ src/main/java/io/sjhello/refactoring/chapter8/splitLoop/People.java
@@ -0,0 +1,31 @@
+package io.sjhello.refactoring.chapter8.splitLoop;
+
+import java.util.List;
+
+public class People {
+	private int age;
+	private int salary;
+
+	public People(int age, int salary) {
+		this.age = age;
+		this.salary = salary;
+	}
+
+	public static void main(String[] args) {
+		List<People> peoples = List.of(new People(12, 500), new People(20, 1000));
+		int youngest = peoples.isEmpty() ? Integer.MAX_VALUE : peoples.get(0).getAge();
+		int totalSalary = 0;
+		for (People people : peoples) {
+			if (people.getAge() < youngest) youngest = people.getAge();
+			totalSalary += people.getSalary();
+		}
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+}
